7a651a1218a5064915c398f23fff8eb818449161
Bertanosomething <bertanosomething@gmail.com>
2025-01-11 11:43:18 +0300

    Removed the Camera Button from MainActivity.

---
+++ app/src/main/java/com/example/staucktion/ApiActivity.java
@@ -0,0 +1,251 @@
+package com.example.staucktion;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.staucktion.api.ApiService;
+import com.example.staucktion.api.RetrofitClient;
+
+import com.example.staucktion.utils.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+
+public class ApiActivity extends AppCompatActivity {
+
+    Button btnHealthCheck;
+    Button btnGoBack;
+    Button btnUploadPhoto;
+    TextView tvApi;
+    TextView tvCode;
+    EditText etBaseUrl;
+    Button btnSetBaseUrl;
+    RetrofitClient retrofitClient;
+    ApiService apiService;
+    MainActivity mainActivity;
+    private static final int REQUEST_CODE_PERMISSION = 1;
+    private static final int REQUEST_CAMERA_CAPTURE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_api);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // connect components
+        btnHealthCheck = findViewById(R.id.btnHealthCheck);
+        btnUploadPhoto = findViewById(R.id.btnUploadPhoto);
+        tvApi = findViewById(R.id.tvApi);
+        tvCode = findViewById(R.id.tvCode);
+        btnGoBack = findViewById(R.id.btnGoBack);
+        etBaseUrl = findViewById(R.id.etBaseUrl);
+        btnSetBaseUrl = findViewById(R.id.btnSetBaseUrl);
+
+        // Request permissions if not already granted
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, REQUEST_CODE_PERMISSION);
+        }
+        //Initializes retrofit to use in creating an API Service
+        retrofitClient = new RetrofitClient();
+        // Create the API Service
+        apiService = retrofitClient.getInstance().create(ApiService.class);
+        etBaseUrl.setText(retrofitClient.getBaseUrl());
+        mainActivity = MainActivity.getInstance();
+
+        btnSetBaseUrl.setOnClickListener(v -> {
+            String newBaseUrl = etBaseUrl.getText().toString();
+            if (!newBaseUrl.isEmpty()) {
+                retrofitClient.setBaseUrl(newBaseUrl);
+                apiService = retrofitClient.getInstance().create(ApiService.class);
+                btnHealthCheck.callOnClick();
+            }
+        });
+
+        // health check button click
+        btnHealthCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("HealthCheck", "HealthCheck button clicked");
+                tvCode.setText("loading");
+                tvApi.setText("loading");
+
+                // Call the GET endpoint
+                Call<ResponseBody> call = apiService.getHealthStatus();
+
+                // Perform the network request
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    // method that gets triggered when the HTTP request made above
+                    // by Retrofit receives a response from the server.
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful() && response.body() != null) {
+                            //if the response has a success code and the body isn't null then we
+                            // move forward with the code. Using the contents of the body to fill
+                            //this health check section
+                            try {
+                                String responseBody = response.body().string();
+                                String responseCode = response.code() + "";
+                                tvApi.setText(responseBody);
+                                tvCode.setText(responseCode);
+                                Log.d("HealthCheck", "Response: " + responseBody);
+                                Log.d("HealthCheck", "Code: " + responseCode);
+                            } catch (Exception e) {
+                                tvApi.setText(e.getMessage());
+                                Log.e("HealthCheck", "Error reading response: " + e.getMessage());
+                            }
+                        } else {
+                            Log.e("HealthCheck", "Request failed. Code: " + response.code());
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        Log.e("HealthCheck", "Network request failed: " + t.getMessage());
+                    }
+                });
+            }
+        });
+
+        // return button click
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
+
+        //This segment commences the GPS checks for the Take/Upload Photo Button
+        btnUploadPhoto.setOnClickListener(v -> {
+            /*Intent cameraIntent = new Intent(this, CameraActivity.class);
+            mainActivity.setIsCameraActive(true);
+            startActivityForResult(cameraIntent, REQUEST_CAMERA_CAPTURE);*/
+            if (mainActivity.getIsGpsEnabled()) {
+                // Open the camera
+                Intent cameraIntent = new Intent(this, CameraActivity.class);
+                // Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // native one
+                PackageManager packageManager = getPackageManager();
+                if (cameraIntent.resolveActivity(packageManager) != null) {
+                    startActivityForResult(cameraIntent, REQUEST_CAMERA_CAPTURE);
+                    mainActivity.setIsCameraActive(true);
+                    mainActivity.setHasGPSTurnedOffOnceWhileInCamera(false);
+                }
+            } else {
+                // Show a warning message
+                Toast.makeText(this, "Please turn on location services to be able to take a photo.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(mainActivity != null) {
+            if(mainActivity.isCameraActivityFinishedProperly(requestCode, resultCode, data)) {
+                String imagePath = data.getStringExtra("image_path");
+                File photoFile = new File(imagePath);
+
+                // Upload photo to server
+                uploadPhoto(photoFile);
+            }
+        } else {
+            Toast.makeText(this, "Something went wrong while handling the camera activity result", Toast.LENGTH_SHORT).show();
+        }
+
+        /*if (requestCode == REQUEST_CAMERA_CAPTURE && resultCode == RESULT_OK) {
+            // Image capture success, get the image path
+            String imagePath = data.getStringExtra("image_path");
+            File photoFile = new File(imagePath);
+
+            // Upload photo to server
+            uploadPhoto(photoFile);
+        } else {
+            Toast.makeText(this, "Failed to capture photo", Toast.LENGTH_SHORT).show();
+        }*/
+    }
+
+
+
+    private void uploadPhoto(File photoFile) {
+        tvApi.setText("loading");
+        tvCode.setText("loading");
+
+        if (photoFile == null) return;
+
+        try {
+
+
+            // Create a RequestBody for the photo
+            RequestBody requestFile = RequestBody.create(MediaType.get("image/*"), photoFile);
+
+            // MultipartBody.Part is used to send the actual file
+            MultipartBody.Part body = MultipartBody.Part.createFormData("photo", photoFile.getName(), requestFile);
+
+            // Call the API
+            Call<ResponseBody> call = apiService.uploadPhoto(body);
+            call.enqueue(new Callback<ResponseBody>() {
+                @Override
+                public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                    if (response.isSuccessful()) {
+                        try {
+                            String responseBody = response.body().string();
+                            String responseCode = response.code() + "";
+                            tvApi.setText(responseBody);
+                            tvCode.setText(responseCode);
+                            Toast.makeText(ApiActivity.this, "Upload successful!", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        Toast.makeText(ApiActivity.this, "Upload failed: " + response.code(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<ResponseBody> call, Throwable t) {
+                    Toast.makeText(ApiActivity.this, "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.d("upload", t.getMessage());
+                }
+            });
+        } catch (Exception e) {
+            Toast.makeText(this, "File error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            Log.d("select", e.getMessage());
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/staucktion/MainActivity.java
@@ -0,0 +1,164 @@
+package com.example.staucktion;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import timber.log.Timber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity instance;
+    private TextView mtv;
+    private Button btnChangeActivity;
+    private LocationManager locationManager;
+    private static final int CAMERA_REQUEST_CODE = 100;
+    private boolean isCameraActive = false;
+    private boolean hasGPSTurnedOffOnceWhileInCamera = false;
+    private final BroadcastReceiver gpsStatusReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            updateGpsStatus(false); // Update GPS status on any change
+        }
+    };
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        instance = this;
+        Timber.plant(new Timber.DebugTree());
+        Timber.i("Starting MainActivity");
+
+        // Link UI elements
+        //Button mbtn = findViewById(R.id.mbtn);
+        btnChangeActivity = findViewById(R.id.btnChangeActivity);
+        mtv = findViewById(R.id.mtv);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // click event on change activity button
+        btnChangeActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ApiActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Register BroadcastReceiver to listen for GPS status changes
+        IntentFilter filter = new IntentFilter(LocationManager.PROVIDERS_CHANGED_ACTION);
+        registerReceiver(gpsStatusReceiver, filter);
+
+        // Initial GPS status check (no Toast)
+        updateGpsStatus(false);
+
+        // Set OnClickListener for the button
+        //This section of code is used to launch the camera using an onClickListener
+        /*mbtn.setOnClickListener(v -> {
+            if (isGpsEnabled()) {
+                // Open the camera
+                Intent cameraIntent = new Intent(this, CameraActivity.class);
+                // Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // native one
+                PackageManager packageManager = getPackageManager();
+                if (cameraIntent.resolveActivity(packageManager) != null) {
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+                    isCameraActive = true;
+                    hasGPSTurnedOffOnceWhileInCamera = false;
+                }
+            } else {
+                // Show a warning message
+                Toast.makeText(MainActivity.this, "Please turn on location services to be able to take a photo.", Toast.LENGTH_LONG).show();
+            }
+        });*/
+        //
+    }
+
+    //Allows these classes to access the current instance of MainActivity
+    // without needing to pass the activity context explicitly everywhere.
+    public static MainActivity getInstance() {
+        return instance;
+    }
+
+    // Method which checks to see if GPS is enabled
+    private boolean isGpsEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    // Method to update GPS status
+    private void updateGpsStatus(boolean showToast) {
+        if (isGpsEnabled()) {
+            mtv.setText(R.string.gps_is_on);
+            Timber.i("GPS is on");
+        } else {
+            mtv.setText(R.string.gps_is_off);
+            Timber.i("GPS is off");
+            if (showToast) {
+                Toast.makeText(MainActivity.this, "GPS is off. Please turn it back on.", Toast.LENGTH_LONG).show();
+            }
+            if(isCameraActive && !hasGPSTurnedOffOnceWhileInCamera) {
+                hasGPSTurnedOffOnceWhileInCamera = true;
+                Timber.i("GPS is off after camera is opened");
+                Intent killIntent = new Intent("com.example.staucktion.KILL_CAMERA_ACTIVITY");
+                sendBroadcast(killIntent);
+            }
+        }
+    }
+
+    // Handle the result of the camera activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (this.isCameraActivityFinishedProperly(requestCode, resultCode, data)) {
+            // Show a success message if the picture was taken successfully
+            String imagePath = data.getStringExtra("image_path");
+            Toast.makeText(this, "Image saved at: " + imagePath, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void setIsCameraActive(boolean isCameraActive) {
+        this.isCameraActive = isCameraActive;
+    }
+
+    public boolean getIsGpsEnabled() {
+        return isGpsEnabled();
+    }
+
+    public void setHasGPSTurnedOffOnceWhileInCamera(boolean hasGPSTurnedOffOnceWhileInCamera) {
+        this.hasGPSTurnedOffOnceWhileInCamera = hasGPSTurnedOffOnceWhileInCamera;
+    }
+
+    //This method makes sure the camera activity finished properly
+
+    public boolean isCameraActivityFinishedProperly(int requestCode, int resultCode, Intent data) {
+        boolean result = false;
+        //Parameter to check the result is coming from the camera activity.
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            // Check if the GPS has been disabled at least once while taking picture after the camera activity finishes
+            if (hasGPSTurnedOffOnceWhileInCamera || !isGpsEnabled()) {
+                // Show a warning if GPS is off
+                Toast.makeText(MainActivity.this, "GPS was off while taking the picture. Please turn it back on.", Toast.LENGTH_LONG).show();
+            } else if(resultCode == RESULT_OK) {
+                // Handling of the result changes based on from which activity it has been called from
+                result = true;
+            }
+            isCameraActive = false;
+            hasGPSTurnedOffOnceWhileInCamera = false;
+        }
+        return result;
+    }
+
+}
